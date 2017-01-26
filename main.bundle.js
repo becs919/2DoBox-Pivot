ac61fd0c2c96cca772f3174ef7a1c0fd933f38b5
Staci McWilliams <stacinicole86@gmail.com>
2017-01-26 14:20:14 -0700

    Added in test for title and task to append to the dom

--- main.bundle.js
+++ main.bundle.js
@@ -46,43 +46,49 @@
 
 	'use strict';
 
+	var $title = $('.title-input');
+	var $task = $('.task-input');
+	var $saveButton = $('.save-button');
+
 	$(document).ready(function () {
 	  for (var i = 0; i < localStorage.length; i++) {
 	    append(JSON.parse(localStorage.getItem(localStorage.key(i))));
 	  }
 	});
+	//for let is low key dangerous. Can cause problems.
+
 
 	// EVENT LISTENERS
-	$('.save-button').on('click', function () {
-	  grabIdea();
+	$saveButton.on('click', function () {
+	  grabToDo();
 	  clearFields();
 	  disableSaveButton();
 	});
 
 	$('.bottom-container').on('click', '.delete-button', function () {
-	  $(this).closest('.idea-section').remove();
-	  var idKey = $(this).closest('.idea-section').attr('id');
+	  $(this).closest('.todo-section').remove();
+	  var idKey = $(this).closest('.todo-section').attr('id');
 	  localStorage.removeItem(idKey);
 	});
 
 	$('.bottom-container').on('click', '.up-vote, .down-vote', function () {
-	  var $getIdea = $(this).closest('.idea-section');
-	  var id = $getIdea.prop('id');
-	  var quality = $getIdea.find('.quality').text();
+	  var getToDo = $(this).closest('.todo-section');
+	  var id = getToDo.prop('id');
+	  var importance = getToDo.find('.importance').text();
 	  var upVoteOrDownVote = $(this).prop('class');
-	  var newQuality = sortButtons(upVoteOrDownVote, quality);
-	  $getIdea.find('.quality').text(newQuality);
+	  var newImportance = sortButtons(upVoteOrDownVote, importance);
+	  getToDo.find('.importance').text(newImportance);
 	  var storedObj = JSON.parse(localStorage.getItem(id));
-	  storedObj.quality = newQuality;
+	  storedObj.importance = newImportance;
 	  storeItem(storedObj);
 	});
 
-	$('.bottom-container').on('blur', '.idea-title, .idea-task', function () {
-	  var id = $(this).closest('.idea-section').prop('id');
-	  var idea = JSON.parse(localStorage.getItem(id));
-	  idea.title = $(this).closest('.idea-section').find('.idea-title').text();
-	  idea.task = $(this).closest('.idea-section').find('.idea-task').text();
-	  localStorage.setItem(id, JSON.stringify(idea));
+	$('.bottom-container').on('blur', '.todo-title, .todo-task', function () {
+	  var id = $(this).closest('.todo-section').prop('id');
+	  var todo = JSON.parse(localStorage.getItem(id));
+	  todo.title = $(this).closest('.todo-section').find('.todo-title').text();
+	  todo.task = $(this).closest('.todo-section').find('.todo-task').text();
+	  localStorage.setItem(id, JSON.stringify(todo));
 	});
 
 	$('.search-field').on('keyup', function () {
@@ -95,8 +101,8 @@
 	});
 
 	$('.title-input, .task-input').on('keyup', function () {
-	  var title = $('.title-input').val();
-	  var task = $('.task-input').val();
+	  var title = $title.val();
+	  var task = $task.val();
 	  if (title.length > 0 && task.length > 0) {
 	    enableSaveButton();
 	  } else {
@@ -105,71 +111,88 @@
 	});
 
 	// GLOBAL FUNCTIONS
-	function Idea(title, task) {
+	function ToDo(title, task) {
 	  this.title = title;
 	  this.task = task;
 	  this.id = Date.now();
-	  this.quality = 'swill';
+	  this.importance = 'normal';
 	}
 
-	function grabIdea() {
-	  var title = $('.title-input').val();
-	  var task = $('.task-input').val();
-	  var idea = new Idea(title, task);
-	  storeItem(idea);
-	  append(idea);
+	function grabToDo() {
+	  var title = $title.val();
+	  var task = $task.val();
+	  var todo = new ToDo(title, task);
+	  storeItem(todo);
+	  append(todo);
 	}
 
-	function append(idea) {
-	  $('ul').prepend('<section id=' + idea.id + ' class="idea-section">\n      <button class=\'delete-button buttons\'>delete</button>\n      <div class="input-text">\n      <li class=\'idea-title\' contenteditable>' + idea.title + '</li>\n      <li class=\'idea-task\' contenteditable>' + idea.task + '</li>\n      </div>\n      <button class=\'up-vote buttons\'>up</button>\n      <button class=\'down-vote buttons\'>down</button>\n      <p>quality: <span class="quality">' + idea.quality + '</span></p>\n    </section>');
-	}
+	function append(todo) {
+	  $('ul').prepend('<section id=' + todo.id + ' class="todo-section">\n      <div class="input-text">\n      <li class=\'todo-title\' contenteditable>' + todo.title + '</li>\n      <li class=\'todo-task\' contenteditable>' + todo.task + '</li>\n      </div>\n      <button class=\'up-vote buttons\'>up</button>\n      <button class=\'down-vote buttons\'>down</button>\n      <button class=\'completed-task\'>Completed Task</button>\n      <p>importance: <span class="importance">' + todo.importance + '</span></p>\n      <button class=\'delete-button buttons\'>delete</button>\n      </section>');
+	};
 
 	function clearFields() {
-	  $('.title-input').val('');
-	  $('.task-input').val('');
+	  $title.val('');
+	  $task.val('');
 	}
 
 	function disableSaveButton() {
-	  $('.save-button').prop("disabled", true);
+	  $saveButton.prop("disabled", true);
 	}
 
 	function enableSaveButton() {
-	  $('.save-button').prop("disabled", false);
+	  $saveButton.prop("disabled", false);
 	}
 
-	function upVote(quality) {
-	  switch (quality) {
-	    case 'swill':
-	      return 'plausible';
-	    case 'plausible':
-	      return 'genius';
+	function upVote(importance) {
+	  switch (importance) {
+	    case 'none':
+	      return 'low';
+	    case 'low':
+	      return 'normal';npm;
+	    case 'normal':
+	      return 'high';
+	    case 'high':
+	      return 'critical';
 	    default:
-	      return 'genius';
-	  };
-	};
+	      return 'critical';
+	  }
+	}
 
-	function downVote(quality) {
-	  switch (quality) {
-	    case 'genius':
-	      return 'plausible';
-	    case 'plausible':
-	      return 'swill';
+	function downVote(importance) {
+	  switch (importance) {
+	    case 'critical':
+	      return 'high';
+	    case 'high':
+	      return 'normal';
+	    case 'normal':
+	      return 'low';
+	    case 'low':
+	      return 'none';
 	    default:
-	      return 'swill';
-	  };
-	};
+	      return 'none';
+	  }
+	}
 
-	function sortButtons(upVoteOrDownVote, quality) {
+	function sortButtons(upVoteOrDownVote, importance) {
 	  if (upVoteOrDownVote === 'up-vote buttons') {
-	    return upVote(quality);
+	    return upVote(importance);
 	  } else {
-	    return downVote(quality);
-	  };
-	};
-
-	function storeItem(idea) {
-	  localStorage.setItem(idea.id, JSON.stringify(idea));
+	    return downVote(importance);
+	  }
 	}
+
+	function storeItem(todo) {
+	  localStorage.setItem(todo.id, JSON.stringify(todo));
+	}
+
+	// COMPLETED TASK BUTTON
+	$('.bottom-container').on('click', '.completed-task', function () {
+	  $(this).closest('.todo-section').find('.input-text').toggleClass('strike-through');
+	  console.log('hey');
+	});
+
+	// On reloading the page the page the completed TODOs should be exempted from the list.
+	// When the user clicks the show completed TODOs The completed TODOs should be loaded back onto the top of the todo list.
 
 /***/ }
 /******/ ]);
